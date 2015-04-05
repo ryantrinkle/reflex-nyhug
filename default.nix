6857224993341d5e4c8aa49c7596171eb4eb21fb
Ryan Trinkle <ryan.trinkle@gmail.com>
2015-04-05 00:00:53 -0400

    Make everything build with nix

--- default.nix
+++ default.nix
@@ -1,27 +1,40 @@
-let # Copied from <nixpkgs/pkgs/development/haskell-modules/lib.nix> so that -I isn't necessary
-    overrideCabal = drv: f: (drv.override (args: args // {
-      mkDerivation = drv: args.mkDerivation (drv // f drv);
-    })) // {
-      overrideScope = scope: overrideCabal (drv.overrideScope scope) f;
-    };
+{}:
 
-    nixpkgs = import ./nixpkgs {config.allowUnfree = true;};
-    extendHaskellPackages = haskellPackages: haskellPackages.override {
-      overrides = self: super: {
-        reflex = self.callPackage ./reflex {};
-        reflex-dom = self.callPackage ./reflex-dom {};
-        ghcjs-dom = overrideCabal super.ghcjs-dom (drv: {
-          src = nixpkgs.fetchgit {
-            url = git://github.com/xionite/ghcjs-dom.git;
-            rev = "2d5a0aa64454f2c084b1a7c53cadb59b274d1386";
-            sha256 = "8450b1a0de67cf6bb6c304c244e211331da8f5befdf92c089498c4394c14fcc2";
-          };
-        });
-      };
-    };
-in rec {
-  inherit nixpkgs;
-  ghc = extendHaskellPackages nixpkgs.pkgs.haskell-ng.packages.ghc7101;
-  ghcjs = extendHaskellPackages nixpkgs.pkgs.haskell-ng.packages.ghcjs;
-  platforms = [ "ghcjs" ] ++ (if !nixpkgs.stdenv.isDarwin then [ "ghc" ] else []);
+let this = import ./common.nix;
+
+in this.nixpkgs.stdenv.mkDerivation rec {
+  name = "reflex-talk";
+  src = ./frontend;
+  backend = import ./backend {};
+  builder = builtins.toFile "builder.sh" ''
+    source "$stdenv/setup"
+
+    cp -r "$src" "$out"
+    chmod -R u+w "$out"
+    cd "$out"
+
+    mkdir -p bin
+    ln -s "$backend/bin/reflex-talk-backend" bin/backend
+
+    ghcjs --make -O2 -isrc src/Main.hs -o static/result
+    closure-compiler --compilation_level=ADVANCED_OPTIMIZATIONS --js static/result.jsexe/all.js > static/result.jsexe/all.min.js
+  '';
+  buildInputs = [
+    (this.ghc.ghcWithPackages (p: with p; [ snap ]))
+    (this.ghcjs.ghcWithPackages (p: with p; [
+      reflex
+      reflex-dom
+      hscolour
+      haskell-src-meta
+      raw-strings-qq
+      twitter-types
+      twitter-types-lens
+      lens
+      lens-aeson
+      http-types
+      network-uri
+    ]))
+    this.nixpkgs.nodejs
+    this.nixpkgs.closurecompiler
+  ];
 }
